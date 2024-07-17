2302b6616f52f9262c3ba78a4e1d6aff7c17c8a7
AdiAmar1 <165654012+AdiAmar1@users.noreply.github.com>
2024-07-17 16:36:13 +0300

    added js for video on hover

--- Public/ProductsPage-Front/products.js
+++ Public/ProductsPage-Front/products.js
@@ -1,6 +1,5 @@
-//CLIENT SIDE
-//PRODUCTS PAGE
-
+// CLIENT SIDE
+// PRODUCTS PAGE
 
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
@@ -12,8 +11,6 @@ function closeNav() {
     document.getElementById("openBtn").style.display = "block"; // Show the button
 }
 
-//adiiiii
-
 function myFunction(textId) {
     var text = document.getElementById(textId);
     if (text.style.display === "none") {
@@ -22,7 +19,6 @@ function myFunction(textId) {
         text.style.display = "none";
     }
 }
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const genres = ["Action", "Adventure", "Arcade", "Multiplayer", "Fantasy", "Racing", "Sports", "Strategy", "Wargame"];
@@ -49,40 +45,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = [
         {
             imgSrc: '../Pictures/game1.webp',
+            videoSrc: '../Videos/ghost-of-tsushima-VIDEO.webm',
             name: 'Tailored Jeans',
             price: '$19.99',
-            
         },
         {
             imgSrc: '../Pictures/game2.jpg',
+            videoSrc: '../Videos/elden-ring-VIDEO.webm',
             name: 'Cool Shirt',
             price: '$29.99',
-          
         },
         {
             imgSrc: '../Pictures/game3.jpeg',
+            videoSrc: '../Videos/destiny-2-VIDEO.webm',
             name: 'Cool Shirt',
             price: '$29.99',
-            
         },
-        {
-            imgSrc: '../Pictures/game3.jpeg',
-            name: 'Cool Shirt',
-            price: '$29.99',
-            
-        },
-        {
-            imgSrc: '../Pictures/game3.jpeg',
-            name: 'Cool Shirt',
-            price: '$29.99',
-            
-        },
-        {
-            imgSrc: '../Pictures/game3.jpeg',
-            name: 'Cool Shirt',
-            price: '$29.99',
-            
-        }
         // Add more product objects as needed
     ];
 
@@ -92,10 +70,24 @@ document.addEventListener('DOMContentLoaded', () => {
     products.forEach(product => {
         const cardClone = document.importNode(cardTemplate, true);
         cardClone.querySelector('.product-image').src = product.imgSrc;
+        cardClone.querySelector('.product-video source').src = product.videoSrc;
         cardClone.querySelector('.product-name').textContent = product.name;
         cardClone.querySelector('.price').textContent = product.price;
-        
 
         cardContainer.appendChild(cardClone);
+    });
+    const imageContainers = document.querySelectorAll('.media-container');
+
+    imageContainers.forEach(container => {
+        const video = container.querySelector('video');
+
+        container.addEventListener('mouseover', function() {
+            video.play();
+        });
+
+        container.addEventListener('mouseout', function() {
+            video.pause();
+            video.currentTime = 0;
+        });
     });
 });
