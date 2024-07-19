797f000cf9d836356307544953e296dec811f101
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-19 16:48:17 +0300

    product.js Initialization Schema MainFrame

--- Models/product.js
+++ Models/product.js
@@ -2,3 +2,26 @@
  *  Server Side - Models - product.js
  *  Product Model Main Schema
  **********************************************************************************************************************/
+
+const mongoose = require('mongoose'); // Import mongoose library
+const Schema = mongoose.Schema; // Import mongoose Schema
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        // Default?
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+    /// Should use more fields according to the Client - Side
+})
