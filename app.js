ee2e1ee400a26dac5365799bca8211060d4e6018
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-19 16:06:57 +0300

    Initialization of Server within the app, Definitions

--- app.js
+++ app.js
@@ -4,3 +4,19 @@
  * Main Server Application File
  *
  **********************************************************************************************************************/
+
+// Import JavaScript Libraries For Project Usage
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors') // MiddleWare
+
+// Config Environment File path
+require('custom-env').env(process.env.NODE_ENV,'./config');
+
+
+// Create a server
+const app = express();
+
+app.use(express.static('public')); // Use Public as static file
+app.use(cors()); // Initialize MiddleWare
