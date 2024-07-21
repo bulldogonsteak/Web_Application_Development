b995ee6af6c15800837100e9c5b3f68ab04c3823
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-21 21:03:08 +0300

    Services - product.js - Create Product asynchronous function implemented (Need to check within Debugging session)

--- Services/product.js
+++ Services/product.js
@@ -5,11 +5,21 @@
 
 const product = require(`../Models/product.js`); // Import product Model for this file
 
-// Create a product
-const createProduct = async (/**********/) => {
-    //const product = await product.create();
-    const product = new product({
-        // TODO Fields required
-    })
-    await product.save();
-}
+// TODO need to check within debugging session about this function
+// Create a product asynchronous function
+const createProduct = async (productData) => {
+    // Try to create a new product object
+    try {
+        // Create a new product object
+        const product = new product({
+            ...productData, // Usage of spread operator which assign all fields accordingly from the productData
+        });
+
+        // Return promise of the product saving in DB
+        return await product.save();
+    } // End of TRY
+
+    catch(error){ // In case of any error
+        throw new Error(`Error creating product: ${error.message}`);
+    }
+}; // End of createProduct function
