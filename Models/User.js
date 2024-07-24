7ae61c8ec48b1660eb73a8015f71ab1ba1f0d9bd
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-24 17:11:17 +0300

    Update User.js
    User.js Model - server side

--- Models/User.js
+++ Models/User.js
@@ -5,9 +5,34 @@
 
 const mongoose = require('mongoose'); // Import mongoose library
 const Schema = mongoose.Schema; // Import mongoose Schema
+require('mongoose-type-email');
 
 const userSchema = new Schema({
-    username: {
+    emailAddress: {
         type: String,
-    }
+        required: true,
+        unique: true,
+    },
+    password: { // User's password // TODO need to add additional encryptions for password high security level
+
+
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String, // select from a list of countries
+        required: true,
+    },
+    agreedToTermsPolicy: { // Have to agree to Terms and Privacy policty
+        type: Boolean,
+        required: true,
+    },
+
+
 })
