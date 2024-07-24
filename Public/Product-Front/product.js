75385ec7fd4c810e82c202a2a0eece74d7a480ec
OrtalNisim <ortalnisim012@gmail.com>
2024-07-24 17:42:35 +0300

    connect the similarproduct to the productpage

---
+++ Public/Product-Front/product.js
@@ -0,0 +1,10 @@
+// מעביר לדף של הפריט הבודד בעת לחיצה על אחד מהפריטים
+//לבדוק אם עובד על מוצר אחר
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableCol = document.getElementById('clickable-col');
+    if (clickableCol) {
+        clickableCol.addEventListener('click', function() {
+            window.location.href = '../Product-Front/product.html';  // Replace with the actual path to your product page
+        });
+    }
+});
