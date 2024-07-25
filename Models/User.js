d272f8c048281d085b20cde3337568a243d948e2
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-25 16:57:51 +0300

    Update User.js
    Regex defined to be unique within user.js model

--- Models/User.js
+++ Models/User.js
@@ -6,12 +6,35 @@
 const mongoose = require('mongoose'); // Import mongoose library
 const Schema = mongoose.Schema; // Import mongoose Schema
 
+/***
+ * @brief:
+ * /^[^\s@]+@[^\s@]+\.com$/ :=
+ * - `^` := Assert the start of the string
+ * - `[^\s@]` := Matches one or more characters that are neither whitespace nor `@`.
+ * - `+` := Concatenate string classes
+ * `.com$` := String ends with the suffix (ending) of .com
+ *
+ * @example:
+ * const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ * const testEmail = (email) => emailRegex.test(email);
+ *
+ * console.log(testEmail("example@example.com")); /// true
+ * console.log(testEmail("invalid-email")); /// false
+ *
+ ***/
+const emailRegex = /^[^\s@]+@[^\s@]+\.com$/; // Regex expression to insure email template is inserted
 
 const userSchema = new Schema({
     emailAddress: { // User emailAddress will be used as a unique key for his account
         type: String,
         required: true,
         unique: true,
+        validate: {
+            validator: (email) => {
+                return emailRegex.test(email);
+            },
+            message: props => `${props.value} is not a valid email address`,
+        }
     },
     password: { // User's password // TODO need to add additional encryptions for password high security level
         type: String,
