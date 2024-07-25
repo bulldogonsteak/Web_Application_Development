a82409ff7b49e0e683d61b126639a6412eabc334
bulldogonsteak <130228700+bulldogonsteak@users.noreply.github.com>
2024-07-25 21:18:59 +0300

    Updated implementation of services User.js - needs to do debugging session

--- Services/User.js
+++ Services/User.js
@@ -3,5 +3,75 @@
  *  User Services Methods
  **********************************************************************************************************************/
 
-// Import User Model
-const User = require('../Models/User');
+// Imported Files
+const User = require('../Models/User'); // Import User Model
+const bcrypt = require('bcrypt'); // Import encryption Library
+
+// Defined Regex according to the Model
+const emailRegex = /^[^\s@]+@[^\s@]+\.com$/; // Email Regex
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,32}$/; // Password Regex
+
+// Function to check validity of email - should be synchronous - needs to finish validation checks and then proceed
+function isEmailValid(email) {
+    return emailRegex.test(email);
+}
+
+// Function to check validity of password
+// Should be synchronous - needs to finish validation checks and then proceed
+function isPasswordValid(password) {
+    return passwordRegex.test(password);
+}
+
+// Function to register a new User to the system
+// Should be asynchronous - not to delay client interaction with the server while the server communicates with the DB
+async function register(userData) {
+
+    // Validity Checks to insure valid Data
+    if (!isEmailValid(userData.emailAddress)) {
+        console.log('UserData (register) Email is invalid'); // TODO self-debugging
+        throw new Error('UserData Email is invalid');
+    }
+
+    if (!isPasswordValid(userData.password)) {
+        console.log('UserData (register) password is invalid'); // TODO self-debugging
+        throw new Error('UserData password is invalid');
+    }
+
+    // Check if the user is already registered
+    const isUserExists = await User.findOne({emailAddress: userData.emailAddress});
+
+    // In Case the user is Exist
+    if (isUserExists) {
+        console.log(`User already exists`); // TODO self-debugging
+        throw new Error(`User already exists`);
+    }
+
+    // The User is new to the system
+    const newUser = new User({userData})
+    await newUser.save(); // Save User's data within DB
+    return newUser; // return the newUser info
+}
+
+// Function to log in a user to the system with the given means of identification
+async function login(emailAddress, password) {
+    // Validity Checks to insure valid Data
+    if (!isEmailValid(emailAddress)) {
+        console.log('UserData (login) Email is invalid'); // TODO self-debugging
+        throw new Error('UserData Email is invalid');
+    }
+
+    if (!isPasswordValid(password)) {
+        console.log('User (login) password is invalid'); // TODO self-debugging
+        throw new Error(`User's password is invalid`);
+    }
+
+    // Checks if the user exists in the system
+    const isUserExists = await User.findOne({emailAddress: emailAddress});
+    return isUserExists != null; // isUserExists != null ? true : false
+}
+
+module.exports = {
+    register,
+    login,
+}
+
